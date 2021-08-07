6617ac8a195e071361b149a37d5572b3a31b2fa7
Michael C. J. Kao <chengjan.kao@gojek.com>
2021-08-07 20:48:59 +0800

    disabled the window customisation since it moves the cursor to different window with c-left/right

--- init.el
+++ init.el
@@ -66,7 +66,7 @@
 (require 'init-minibuffer)
 (require 'init-hippie-expand)
 (require 'init-company)
-(require 'init-windows)
+;; (require 'init-windows)
 (require 'init-sessions)
 (require 'init-mmm)
 
