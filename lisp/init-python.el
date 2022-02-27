6b787c3188da01792cb8039acf39e49234254652
Kao Cheng Jan <chengjan.kao@gojek.com>
2022-02-27 17:24:08 +0800

    enable auto-pyenv to work with pyenv

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -10,6 +10,7 @@
   '(better-defaults                 ;; Set up some better Emacs defaults
     elpy                            ;; Emacs Lisp Python Environment
     material-theme                  ;; Theme
+    flycheck
     )
   )
 
@@ -18,7 +19,14 @@
                 ("SConscript\\'" . python-mode))
               auto-mode-alist))
 
-(setq python-shell-interpreter "python3")
+(setq python-shell-interpreter "python")
+;; based on: https://adamoudad.github.io/posts/emacs/docker-python-shell-emacs/
+;;
+;; however, it results in "input device is not a tty" error. This
+;; doesn't change even if we use the same image used in the original
+;; source.
+;;
+;; (setq python-shell-interpreter "/Users/user/Git/mk_experiment/docker_interpretor/start_docker_interpretor.sh")
 
 (require-package 'pip-requirements)
 
@@ -42,15 +50,42 @@
 (when (maybe-require-package 'toml-mode)
   (add-to-list 'auto-mode-alist '("poetry\\.lock\\'" . toml-mode)))
 
-(when (maybe-require-package 'reformatter)
-  (reformatter-define black :program "black"))
+;; (when (maybe-require-package 'reformatter)
+;;   (reformatter-define black :program "black"))
 
 ;; disable warning when starting python-interpreter
 ;; (setq python-shell-completion-native-enable 0)
 (setq python-shell-completion-native-enable nil)
+;; (add-to-list 'python-shell-completion-native-disabled-interpreters
+;;              "python")
+
+;; enable eldoc mode
+(add-hook 'python-mode-hook '(lambda () (eldoc-mode 1)) t)
 
 ;; Enable elpy
+(require 'pyenv-mode-auto)
 (elpy-enable)
+;; (setenv "WORKON_HOME" "~/.pyenv/versions/")
+;; (setq elpy-rpc-backend "jedi")
+;; (setq python-shell-interpreter "~/.pyenv/shims/python3")
+;; (setq elpy-rpc-python-command "~/.pyenv/shims/python3")
+;; (setq elpy-rpc-python-command 'current)
+;; (setq elpy-rpc-virtualenv-path "~/.pyenv/versions/plan-scorer-data-pipeline")
+;; (setq elpy-rpc-virtualenv-path "/usr/local/Cellar/pyenv-virtualenv/1.1.5/shims")
+
+;; Enable Flycheck
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+;; (use-package elpy
+;;    :config
+;;    (package-initialize)
+;;    (elpy-enable)
+;;    (setenv "WORKON_HOME" "~/.pyenv/versions/")
+;;    (setq elpy-rpc-backend "jedi")
+;;    (setq python-shell-interpreter "~/.pyenv/shims/python3")
+;;  )
 
 (provide 'init-python)
 ;;; init-python.el ends here
+
