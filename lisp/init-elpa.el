6d2413362b4c3755d5d9884343fc7cba99653f32
Kao Cheng Jan <chengjan.kao@gojek.com>
2022-02-27 17:26:19 +0800

    add melpa

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -14,8 +14,9 @@
 
 
 ;;; Standard package repositories
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
-(add-to-list 'package-archives '( "melpa" . "https://melpa.org/packages/") t)
 ;; Official MELPA Mirror, in case necessary.
 ;;(add-to-list 'package-archives (cons "melpa-mirror" (concat proto "://www.mirrorservice.org/sites/melpa.org/packages/")) t)
 
